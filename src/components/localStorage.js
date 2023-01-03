163ebcab7a9d477ad183feec1d06234fba4cfc67
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-03 22:23:17 +0100

    LocalStorage

--- src/components/localStorage.js
+++ src/components/localStorage.js
@@ -1,17 +1,17 @@
 
-export const todoInLS = (todo) => {
-  localStorage.setItem("todo", JSON.stringify(todo));
-};
+// export const savedTodoToLS = (todo) => {
+//   localStorage.setItem("todo", JSON.stringify(todo));
+// };
 
-export const addTodoInLS = () => {
-  const todoStr = localStorage.getItem("todo");
+// export const loadTodoInLS = () => {
+//   const todoStr = localStorage.getItem("todo");
 
-  if (!todoStr) return;
+//   if (!todoStr) return;
 
-  const todoObj = JSON.parse(todoStr);
-  return todoObj;
-};
+//   const todoObj = JSON.parse(todoStr);
+//   return todoObj;
+// };
 
-export const deleteTodoInLS = () => {
-localStorage.removeItem("todo");
-}
+// export const deleteTodoInLS = () => {
+// localStorage.removeItem("todo");
+// }
