4bab6120b555dd710b99b89bcb7322a022c0596e
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-08 19:47:45 +0100

    Todo-Redux-Toolkit

--- src/components/todos/Todo-RT.jsx
+++ src/components/todos/Todo-RT.jsx
@@ -2,25 +2,26 @@ import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo, removeTodo } from "../../redux/slices/todos/todoRTSlice";
 import "./Todos.scss";
+import {FaTrashAlt} from 'react-icons/fa'
 
 export const TodoRT = () => {
-  const [newTodo, setNewTodo] = useState('');
-
-  // const todos = useSelector((state) => state.todoRT.todos);
-  // const count = useSelector((state) => state.todoRT.count)
-  const { count, todos } = useSelector((state) => state.todoRT);
+  const { todosRT, count } = useSelector((state) => state.todoRT);
   const dispatch = useDispatch();
+  const [todoRT, setNewTodoRT] = useState("");
+
+  // const todosRT = useSelector((state) => state.todoRT.todosRT);
+  // const count = useSelector((state) => state.todoRT.count)
   const inputRef = useRef();
 
   const handleAddTodo = (e) => {
     e.preventDefault();
-    dispatch(addTodo(newTodo));
-    setNewTodo("");
+    dispatch(addTodo(todoRT));
+    setNewTodoRT("");
     inputRef.current.focus();
   };
-  const handleRemoveTodo = (id) => {
-    dispatch(removeTodo(id));
-  };
+  // const removeTodo = (id) => {
+  //   dispatch(removeTodo(id));
+  // };
   return (
     <div className="Todo">
       <h2>Todo-Redux-Toolkit</h2>
@@ -29,22 +30,23 @@ export const TodoRT = () => {
           ref={inputRef}
           type="text"
           placeholder="Todo"
-          value={newTodo}
-          // value={todos.title}
-          onChange={(e) => setNewTodo(e.target.value)}
+          value={todoRT}
+          // value={todosRT.title}
+          onChange={(e) => setNewTodoRT(e.target.value)}
         />
         <button type="submit">Add</button>
       </form>
       <div className="todos">
         {count > 0 &&
-          todos.map((todo, index) => (
+          todosRT.map((todo, index) => (
             <div key={todo.id} className="todo">
               <div className="title">
                 {index + 1}
                 {":"} {todo.title}
               </div>
-              <button onClick={() => handleRemoveTodo(todo.id)}>Delete</button>
-              {/* <button onClick={()=> dispatch(removeTodo(todo.id))}>Delete</button> */}
+              {/* <button onClick={() => removeTodo(todo.id)} role="button"> */}
+              <FaTrashAlt  onClick={() => dispatch(removeTodo(todo.id))}
+                role="button" />
             </div>
           ))}
         {count === 0 && <p>No todos</p>}
