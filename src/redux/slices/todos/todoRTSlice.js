daae7d08f3fdd8e2faf6a781a013697603627ece
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-08 19:48:17 +0100

    Update

--- src/redux/slices/todos/todoRTSlice.js
+++ src/redux/slices/todos/todoRTSlice.js
@@ -1,22 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { storeTodoToLS, loadTodoInLS, deleteTodoInLS } from "../../../components/localStorage";
-
-// const saveTodoInLS = localStorage.getItem("updateTodoState")
-//   if (saveTodoInLS) {
-//     initialState = JSON.parse(saveTodoInLS)
-//   } else {
-//     return []
-//   }
-//   console.log(saveTodoInLS)
-
 
 const initialState = {
-  todos: [],
+  todosRT: [],
   count: 0,
 };
 
 export const todoRTSlice = createSlice({
-  name: "todoRT",
+  name: "todosRT",
   initialState,
   reducers: {
     addTodo: (state, action) => {
@@ -24,13 +14,13 @@ export const todoRTSlice = createSlice({
         id: new Date().toString(),
         title: action.payload,
       };
-      state.todos.push(todo);
+      state.todosRT.push(todo);
       state.count += 1;
-      localStorage.setItem("todos", JSON.stringify(state.todos));
-      
     },
     removeTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      state.todosRT = state.todosRT.filter(
+        (todo) => todo.id !== action.payload
+      );
       state.count -= 1;
     },
   },
