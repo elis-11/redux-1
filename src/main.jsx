e43594761e2a5180384f88bc53373dd1d940c1db
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-11 15:39:59 +0100

    Redux-Toolkit - LocalStorage!!!!!!!! - Redux-Persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
 import sliceA from "./slices/counts/sliceA";
 import sliceB from "./slices/counts/sliceB";
 import sliceC from "./slices/counts/sliceC";
@@ -6,21 +7,32 @@ import counter from "./slices/counterSlice";
 import todoRT from "./slices/todos/todoRTSlice";
 import cruds from "./slices/todos/crudSlice";
 import items from "./slices/todos/reduxTolSlice";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  sliceA,
+  sliceB,
+  sliceC,
+  counter,
+  todoRT,
+  cruds,
+  items,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    sliceA,
-    sliceB,
-    sliceC,
-    counter,
-    todoRT,
-    cruds,
-    items,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+export const persistor = persistStore(store)
 
 // console.log(store);
