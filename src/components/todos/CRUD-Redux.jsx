258220a3b832eaf3549576c02fac981c5ea783d6
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-11 17:51:21 +0100

    Crud - fixed

--- src/components/todos/CRUD-Redux.jsx
+++ src/components/todos/CRUD-Redux.jsx
@@ -1,75 +1,97 @@
-import { useRef, useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
-import { useDispatch, useSelector } from "react-redux";
-import { addCrud } from "../../redux/slices/todos/crudSlice";
+import { useEffect, useRef, useState } from "react";
 import "./Todos.scss";
-
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 export const CrudRedux = () => {
-  // const [cruds, setCruds] = useState([]);
-  const { cruds, count } = useSelector((state) => state.cruds);
-  const dispatch = useDispatch();
-  // const [newCrud, setNewCrud] = useState("");
-
-  const [newCrud, setNewCrud] = useState({ title: "", description: "" });
-  const inputRef = useRef();
-
-  const addTodo = () => {
-    // e.preventDefault();
-    dispatch(addCrud(newCrud));
-    setNewCrud({ ...newCrud, title: "", description: "" });
-    inputRef.current.focus();
-  };
-
-  // const handleAddTodo = (e) => {
-  //   setNewCrud({ ...newCrud, [e.target.name]: e.target.value });
-  // };
-
-  const handleDeleteTodo = (id) => {
-    setCruds(cruds.filter((todo) => todo.id !== id));
-    // const deletePerson = cruds.filter((todo) => todo.id !== id);
-    // setCruds(deletePerson);
-  };
-
-  return (
-    <div className="Todo">
-      <h2>CRUD-Redux</h2>
-      <p>This is my first redux-todo y'all!</p>
-
-      <div className="add">
-        <input
-          ref={inputRef}
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={newCrud.title}
-          // onChange={handleAddTodo}
-          onChange={(e) => setNewCrud(e.target.value)}
-        />
-        <input
-          type="text"
-          name="description"
-          placeholder="Description"
-          value={newCrud.description}
-          onChange={(e) => setNewCrud(e.target.value)}
-        />
-        <button onClick={addTodo}>+</button>
-        {/* <button type="submit">+</button> */}
-      </div>
-
-      <div className="todos">
-        {cruds.map((crud, index) => (
-          <div key={crud.id} className="todo">
-            <div className="title">
-              {index + 1}: {crud.title}
+  
+    const inputRef = useRef()
+  
+    const [todos, setTodos] = useState(()=>{
+      const savedTLS = localStorage.getItem('todo-crud')
+      if(savedTLS) {
+            return JSON.parse(savedTLS)
+      } else {
+        return [] }
+    });
+  
+    useEffect(()=>{
+      localStorage.setItem('todo-crud', JSON.stringify(todos))
+    }, [todos])
+  
+    const [newTodo, setNewTodo] = useState({
+      title: "",
+      description: "",
+    });
+  
+    const addTodo = () => {
+      const todoNewState = {
+        id: Date.now().toString(),
+        title: newTodo.title,
+        description: newTodo.description,
+      };
+      setTodos([...todos, todoNewState]);
+      setNewTodo({ ...newTodo, title: "", description: "" });
+      inputRef.current.focus()
+    };
+    const handleAddTodo = (e) => {
+      setNewTodo({ ...newTodo, [e.target.title]: e.target.value });
+    };
+  
+    const handleDelete = (id) => {
+      const deletePerson = todos.filter((todo) => todo.id !== id);
+      setTodos(deletePerson);
+    };
+  
+    return (
+      <div className="Todo">
+        <h2>Todo-CRUD</h2>
+        <h3 style={{ marginTop: "1px" }}>
+          {todos.length} List {todos.length === 1 ? "Todo" : "Todos"}
+        </h3>
+        <div className="add">
+          <input
+            ref={inputRef}
+            type="text"
+            title="title"
+            placeholder="Title"
+            value={newTodo.title}
+            onChange={handleAddTodo}
+          />
+          <input
+            type="text"
+            title="description"
+            placeholder="Description"
+            value={newTodo.description}
+            onChange={handleAddTodo}
+          />
+          <button onClick={addTodo}>Add</button>
+        </div>
+  
+          {todos.length ? (
+            <div className="todos">
+              
+              {todos.map((todo) => (
+                <div key={todo.id} className="todo">
+                  <div className="title">{todo.title}</div>
+                  <div className="description">{todo.description}</div>
+                  <div className="icons">
+                    <FaEdit className="icon" role="button" tabIndex="0" />
+                    <FaTrashAlt
+                      onClick={() => handleDelete(todo.id)}
+                      className="icon"
+                      role="button"
+                      tabIndex="0"
+                    />
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="description">{crud.description}</div>
-            <FaTrashAlt
-              type="button"
-              onClick={() => handleDeleteTodo(crud.id)}
-            />
-          </div>
-        ))}
+          ) : (
+            <p
+              style={{ marginTop: "1rem", textAlign: "center", color: "grey" }}
+            >
+              Your List is empty
+            </p>
+          )}
       </div>
-    </div>
-  );
-};
+    );
+  };  
